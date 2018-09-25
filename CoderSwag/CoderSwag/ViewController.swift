59fb5406a097edc9ae145d69ac84ee7092b81640
Craig Newcomb <crgnewcomb@gmail.com>
2018-09-25 17:11:11 -0500

    initial commit

---
+++ CoderSwag/CoderSwag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoderSwag
+//
+//  Created by Craig Newcomb on 9/25/18.
+//  Copyright © 2018 Craig Newcomb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
